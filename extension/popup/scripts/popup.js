8236a49cc2bfdb379d4e34cf4f60888580cdee45
Harshit Maurya <hmaurya999@gmail.com>
2018-10-02 01:57:50 +0530

    Integrate copy function

--- extension/popup/scripts/popup.js
+++ extension/popup/scripts/popup.js
@@ -1,5 +1,11 @@
-window.onload = function() {
-  document.getElementById("generate").onclick = function() {
-    getData.load();
-  };
+window.onload = function () {
+    const cpBtn = document.getElementById('copy');
+    document.getElementById('generate').onclick = function () {
+        getData.load();
+        cpBtn.innerText = "Copy";
+    };
+    document.getElementById('copy').onclick = function () {
+        copyToClipboard(document.getElementById('output').value);
+        cpBtn.innerText = "Copied";
+    };
 };
