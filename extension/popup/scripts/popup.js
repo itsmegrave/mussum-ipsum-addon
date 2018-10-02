25f5be6b83fa333db25f33aaf414596b56a0183f
Samuel Grave <SamuelGrave@users.noreply.github.com>
2018-10-02 07:51:16 -0300

    Revert "Copy feature implemented with bug fix in num input"

--- extension/popup/scripts/popup.js
+++ extension/popup/scripts/popup.js
@@ -1,21 +1,5 @@
-window.onload = function () {
-    const copyBtn = document.getElementById('copy');
-    const genBtn = document.getElementById('generate');
-    genBtn.onclick = function () {
-        getData.load();
-        copyBtn.innerText = "Cópia";
-    };
-    copyBtn.onclick = function () {
-        copy();
-        copyBtn.innerText = "Copiado";
-    };
+window.onload = function() {
+  document.getElementById("generate").onclick = function() {
+    getData.load();
+  };
 };
-
-function copy() {
-    const textarea = document.getElementById('output');
-    textarea.select();
-    document.execCommand("copy");
-    textarea.selectionEnd = textarea.selectionStart;
-    textarea.blur();
-    window.getSelection().removeAllRanges();
-}
